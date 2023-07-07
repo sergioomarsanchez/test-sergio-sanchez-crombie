83f04824b64cf7ff027a286e79f009da64dfd5b0
Sergio Omar Sánchez <sanchez.omar.sergio@gmail.com>
2023-07-06 21:50:36 -0300

    adding functionality and style to navBar, logos and img added

--- src/app/components/Item.tsx
+++ src/app/components/Item.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Item ({ name, beds }: { name: string, beds: number }) {
   return (
-    <div className='flex justify-between hover:bg-blue-400 transition-colors transition-300ms w-[300px] md:w-[700px]'>
+    <div className='flex justify-between hover:bg-blue-400 transition-colors transition-300ms py-1 my-1 w-[300px] h-12 md:w-[650px] md:h-fit'>
       <div>
         <input id={name} type='checkbox' />
         <label htmlFor={name} className='cursor-pointer m-1'>{name}</label>
--- src/app/components/SideMenu.tsx
+++ src/app/components/SideMenu.tsx
@@ -1,16 +1,25 @@
 'use client'
 import React, { useState } from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import Logo from '../img/Logo.png'
+import LogoSmall from '../img/LogoSmall.png'
+import PracticesLogo from '../icons/PracticesLogo.png'
 
 function SideMenu () {
   const [menuIsOpen, setMenuIsOpen] = useState<Boolean>(true)
   return (
-    <nav className={`h-screen w-20 ${menuIsOpen ? 'w-40' : 'w-20'} fixed left-0 bg-blue-500`}>
-      <span onClick={() => setMenuIsOpen(!menuIsOpen)}>X</span>
+    <nav className={`h-screen w-20 ${menuIsOpen ? 'w-40' : 'w-20'} fixed left-0 bg-navBar-00 flex flex-col gap-5 justify-start items-center pt-5`}>
+      <div className='h-20'>
+        <Image src={menuIsOpen ? Logo : LogoSmall} alt='Logotype of the company' className={`${menuIsOpen ? 'p-5' : 'p-1 mt-5'}`} />
+      </div>
       <ul>
-        <li onClick={() => setMenuIsOpen(false)}>
-          <Link href='/Search' passHref>
-            Practices
+        <li onClick={() => setMenuIsOpen(!menuIsOpen)}>
+          <Link href='/Search' passHref className='flex gap-1'>
+            <Image src={PracticesLogo} alt='Logotype of the company' className='w-6 mx-2' />
+            <span className={`${menuIsOpen ? '' : 'hidden'}`}>
+              Practices
+            </span>
           </Link>
         </li>
       </ul>
